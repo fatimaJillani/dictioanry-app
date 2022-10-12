e5ef6b04d1cb18e597b9b6249fe668e95a1fffd8
fatimaJillani <106700215+fatimaJillani@users.noreply.github.com>
2022-10-11 20:49:05 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello</header>
+      <a href="/" className="btn btn-primary">
+        Code
+      </a>
+    </div>
+  );
+}
+
+export default App;
